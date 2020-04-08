e57441e55ea196682738cc8a82219a93cf0c71ce
Dayniel Alvarez Batista <61252871+daynielbatista@users.noreply.github.com>
2020-04-08 22:54:31 +0100

    Saving small proyect

---
+++ app.js
@@ -0,0 +1,51 @@
+
+
+    
+var zoom = function(){
+    var thumb = document.getElementById("thumb");
+
+    if(thumb.className=="thumb"){
+        thumb.className="thumb grande";
+    }
+    else{
+        thumb.className="thumb";
+    }
+}
+
+/*document.write("<h1>Hello World</h1>")
+
+console.log(['joe', 'ryan', 'day'])
+
+console.log({"username": "ryan", "score": 70.4});
+
+var name = "Pepe";
+let lastName = "Carter";
+const PI = 3.14;
+//camelcase
+let nombreDePersona = "Dany";
+
+document.write(name, lastName)
+
+let n1 = 30;
+let n2 = 20;
+let result = n1 + n2;
+
+let result1 = n1 <= n2;
+console.log(result1);
+
+//concatenacion
+console.log(name + lastName);
+
+let pass = '123'
+let input = '123'
+
+let result2 = input == pass;
+
+if (result2 == true) {
+    console.log('logon correcto');
+}
+else {
+    console.log('Login incorrecto');
+}*/
+
+
